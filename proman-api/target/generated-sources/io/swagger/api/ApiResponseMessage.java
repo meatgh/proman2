d0b2ad3e2cd8011be818a91dd71d237f7b138733
Rahman <rahmanmuti@gmail.com>
2020-06-21 23:43:11 +0530

    DB created

--- proman-api/target/generated-sources/io/swagger/api/ApiResponseMessage.java
+++ proman-api/target/generated-sources/io/swagger/api/ApiResponseMessage.java
@@ -2,7 +2,7 @@ package io.swagger.api;
 
 import javax.xml.bind.annotation.XmlTransient;
 
-@javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2020-06-21T20:41:04.633+05:30")
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2020-06-21T23:19:21.702+05:30")
 
 @javax.xml.bind.annotation.XmlRootElement
 public class ApiResponseMessage {
